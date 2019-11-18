6536613aa52b5af9b2f1c2b9f620eda192895cc0
Martin <m.b.lukac@gmail.com>
2019-11-18 09:09:39 +0000

    improvements

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # occupmobnet
-Occupational Mobility Network in Europe
+## Occupational Mobility Network in Europe
+
+A project aiming to reconstruct occupational mobility networks for European labour markets. Project commenced in **October 2019** and first results will follow soon!
+
+_Use note:_
+We used data from EU Statistics on Income and Living Conditions (EU-SILC) and EU Labour Force Survey (EU-LFS). Each dataset is loaded and analysed separately---files ocm_eusilc.R and ocm_lfs.R shall be used on respective datasets.
+
+
--- ocm_lfs.R
+++ ocm_lfs.R
@@ -5,27 +5,51 @@
 # Martin Lukac @mblukac
 ###############################################################################
 source("libraries.R")
-# data/LFS/Omnibus Data Folder
 
 setwd("/Users/martinlukac/Desktop/Academic Projects/git-Occupational Mobility Network/")
+
 read_in <- function(country){
-  #country <- "AT"
-  setwd(paste0("data/LFS/Omnibus Data Folder/", country))
+  
+  # Save old WD and set to location of files
+  oldwd <- file.path(getwd())
+  setwd(file.path(paste0(oldwd, "/data/LFS/Omnibus Data Folder/", country)))
+  
+  # Get a files list and load in all country files
   list.files() %>%
     llply(read.csv, header = T) %>%
     llply(subset, select = c(COUNTRY,
                              REFYEAR,
+                             IS881D,
+                             IS883D,
                              ISCO1D,
                              ISCO3D, 
-                             ISCOPR1D, 
-                             ISCOPR3D,
                              COEFF
     )) %>%
-    rbindlist(fill = TRUE) -> temp
-  return(temp)
+    rbindlist(fill = TRUE) -> country_dataset
+  
+  # reset WD
+  setwd(file.path(oldwd))
+  
+  # Return the final dataset
+  return(country_dataset)
 }
 
-AT <- read_in("AT")
+# Load it all country codes from dir names
+all_cntrs <- list.files("data/LFS/Omnibus Data Folder")
+# Read all country datafiles in
+for(i in 1:length(all_cntrs)){
+  message(paste0("=== Reading in ", country, " === Progress ", 
+                 i, "/", length(all_cntrs), " ==="))
+  assign(all_cntrs[i], read_in(all_cntrs[i]))
+}
+
+
+# Problem with change in ISCO coding
+at_isco <- names(table(AT$ISCO3D))
+at_is88 <- names(table(AT$IS883D))
+
+intersect(at_isco, at_is88)
+setdiff(at_isco, at_is88)
 
 
 ## Try BHPS and Understanding Society
@@ -33,3 +57,4 @@ us1 <- import("/Users/martinlukac/Desktop/Projects/OLD Academic Projects/BHPS an
 us2 <- import("/Users/martinlukac/Desktop/Projects/OLD Academic Projects/BHPS and Understanding Society Data/Understanding Society/spss/spss19/e_empstat.sav")
 
 bhps1 <- import("/Users/martinlukac/Desktop/Projects/OLD Academic Projects/BHPS and Understanding Society Data/BHPS/spss/spss12/ajobhist.sav")
+
